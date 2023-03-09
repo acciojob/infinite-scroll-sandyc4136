a252d1eef6c04bfe5310637865e3e9ba1f989082
Sandip Chhetry <sandyc4136@gmail.com>
2023-03-09 12:39:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //your code here!
-
+const list=document.querySelector('#infi-list');
+var index=0;
+for(var i=1;i<=10;i++){
+	var item=list.createElement('li');
+	item.innerText="Item" + i;
+	list.appendChild(item);
+});
